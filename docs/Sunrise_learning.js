14c83bbddacad1b69d14f6d10912355407d08c90
nimiko24 <68658629+nimiko24@users.noreply.github.com>
2021-03-13 23:00:59 -0500

    To_up!

---
+++ docs/Sunrise_learning.js
@@ -0,0 +1,10 @@
+function user() {
+    var n = document.getElementById("input_username").value;
+    var p = document.getElementById("input_password").value;
+    if (n == "PeAch0x11" && p == "$0x15docName") {
+        document.getElementById("answer_box").innerHTML = "j6zQ32F9hK7zs5gB5ap32";
+    }
+    else {
+        document.getElementById("answer_box").innerHTML= " ";
+    }
+}
